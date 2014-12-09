486c4ef074b022553999a4e23a8d18dc12838273
Joey <lma_ma@163.com>
2014-12-09 16:40:17 +0800

    初始化

---
+++ src/com/taixin/boxassistant/live/adapter/EpgAdapter.java
@@ -0,0 +1,19 @@
+package com.taixin.boxassistant.live.adapter;
+
+import java.util.List;
+import java.util.Map;
+
+import android.content.Context;
+import android.widget.SimpleAdapter;
+
+public class EpgAdapter extends SimpleAdapter{
+
+	public EpgAdapter(Context context, List<? extends Map<String, ?>> data,
+			int resource, String[] from, int[] to) {
+		super(context, data, resource, from, to);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
